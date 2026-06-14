e7602170aa9a42c579fddef2601ae1667ae9ea2e
Andy <04andyle@gmail.com>
2026-06-14 15:57:04 +0200

    Added EF core packages and AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Models;
+
+namespace TaskApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        :base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks {get; set;}
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
